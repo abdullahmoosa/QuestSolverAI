132a3b1723fe9db01e0b3d804664c7094d9482a9
Abdullah Muhammad Moosa <abdullahmoosa128@gmail.com>
2024-09-30 16:29:33 +0600

    fix unwanted commits

--- backend/services/answer_generation.py
+++ backend/services/answer_generation.py
@@ -1,13 +1,13 @@
 from langchain_community.document_loaders import UnstructuredWordDocumentLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter, SentenceTransformersTokenTextSplitter
-from langchain_community.embeddings import OllamaEmbeddings
+from langchain_openai import ChatOpenAI
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain.prompts import ChatPromptTemplate, PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_community.chat_models import ChatOllama
 from langchain_core.runnables import RunnablePassthrough
 from langchain.retrievers.multi_query import MultiQueryRetriever
 from langchain_openai import AzureOpenAIEmbeddings, AzureChatOpenAI
+from langchain_openai import OpenAIEmbeddings
 from langchain.chains import RetrievalQA
 from ..config import Config
 import os
@@ -29,7 +29,7 @@ def load_docx(file_path):
 
 
 def split_documents(documents):
-    text_splitter = SentenceTransformersTokenTextSplitter(chunk_overlap=200)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=3000,chunk_overlap=1500)
     chunks = text_splitter.split_documents(documents)
 
     return chunks
@@ -59,23 +59,34 @@ def create_question_extraction_pipeline(vectorstore, llm):
 def setup_llm():
     # local_model = "llama3.1:latest"
     llm = AzureChatOpenAI(
-            deployment_name=Config.AZURE_OPENAI_DEPLOYMENT,
-            azure_endpoint=Config.AZURE_OPENAI_ENDPOINT,
-            openai_api_version=Config.AZURE_OPENAI_API_VERSION,
-            openai_api_key=Config.AZURE_OPENAI_API_KEY,
-            temperature= 0.00001
-        )
+        deployment_name=os.environ.get("AZURE_OPENAI_DEPLOYMENT"),
+        azure_endpoint=os.environ.get("AZURE_OPENAI_ENDPOINT"),
+        openai_api_version=os.environ.get("AZURE_OPENAI_API_VERSION"),
+        openai_api_key=os.environ.get("AZURE_OPENAI_API_KEY"),
+        temperature= 0,
+    )
     return llm
 
 def answer_questions(qa_chain,input):
-    query = f"""
-    [INST] Based on the provided context, answer the question. query: {input} [/INST]
     """
-    result = qa_chain({"query": query})
+    Given a question and context from a document, use the question answer chain
+    to generate an answer and its source context.
+
+    Args:
+        qa_chain: The question answer chain
+        input: The question to ask
+
+    Returns:
+        A dictionary containing the answer and its source context
+    """
+    query = f"""
+    [INST] You are a question answering chatbot. Based on the provided context, first analyze the whole context of the document including headings and sub headings of each chunk and then answer the question. Provide source of the answer.question: {input} [/INST]
+    """
+    result = qa_chain.invoke({"query": query})
     return result["result"]
 
 def main():
-    file_path = "/Users/relisource/Personal Projects/QuestSolver/docs/BSBFIN501 Student Assessment Tasks.docx"
+    file_path = "/Users/relisource/Personal Projects/QuestSolver/docs/BSBFIN501 Student Guide.docx"
 
     documents = load_docx(file_path)
     # print(f"documents : {documents}")
@@ -85,11 +96,11 @@ def main():
 
     start_time = time.time()
     qa_chain = create_question_extraction_pipeline(vectorstore, llm)
-    answer = answer_questions(qa_chain,"List three types of budgets.")
+    question = "List three key features of A New Tax System (GST) Act 1999."
+    answer = answer_questions(qa_chain,question)
+    print(f"question : {question}")
     print(f"answer : {answer}")
     end_time = time.time()
-    # print("Extracted Questions:")
-    # print(questions)
     print(f"Time spent: {end_time-start_time} seconds")
 
 
